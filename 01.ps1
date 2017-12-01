acfb63c78273f849d7fb43747748bfa1b66173f1
blue <playbynumbers@gmail.com>
2017-12-01 22:26:52 +0000

    01: Basic functionality
    Passes the first test (1122), but won't compare the last character to
    the first.

---
+++ 01.ps1
@@ -0,0 +1,18 @@
+param (
+    [Parameter(Mandatory=$true)]
+    [string]$puzzleinput
+    )
+
+$acc = 0
+
+for ($i=0; $i -lt $puzzleinput.length; $i++) {
+    $current = $puzzleinput[$i]
+    $next = $puzzleinput[$i+1]
+
+    if ($current -eq $next) {
+        # $current is a char, so cast to string first
+        $acc += [string]$current
+    }
+}
+
+$acc
