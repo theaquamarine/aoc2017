5efce55639e0457aa1d08d009b228a71bae108c6
blue <playbynumbers@gmail.com>
2017-12-01 22:29:26 +0000

    01: Compare last character to the first
    Now passes all tests (1111, 1234, 91212129)

--- 01.ps1
+++ 01.ps1
@@ -7,7 +7,7 @@ $acc = 0
 
 for ($i=0; $i -lt $puzzleinput.length; $i++) {
     $current = $puzzleinput[$i]
-    $next = $puzzleinput[$i+1]
+    $next = $puzzleinput[($i+1) % $puzzleinput.length]
 
     if ($current -eq $next) {
         # $current is a char, so cast to string first
