08fd7b1334c6d888159f0697d78bbc2ba5b0acce
blue <playbynumbers@gmail.com>
2017-12-03 09:22:44 +0000

    02: Part One done

--- 02a.ps1
+++ 02a.ps1
@@ -3,4 +3,15 @@ param (
     [string]$puzzleinput
     )
 
-$puzzleinput -split(' ')| Measure-Object -Minimum -Maximum | select @{Name="Range"; Expression={$_.Maximum - $_.Minimum}}
+function Get-Range {
+    param (
+        [Parameter(Mandatory=$true,ValueFromPipeline)]
+        [string]$line
+        )
+
+    -split $line| Measure-Object -Minimum -Maximum | %{$_.Maximum - $_.Minimum}
+}
+
+$puzzleinput -split '\n' | %{Get-Range $_} | Measure-Object -Sum
+
+# Part One answer: 45158
