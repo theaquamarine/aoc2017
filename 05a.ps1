732a19b5b7a853d362e1bdbd640696ef196f749a
blue <playbynumbers@gmail.com>
2017-12-10 17:55:42 +0000

    get cute

--- 05a.ps1
+++ 05a.ps1
@@ -2,17 +2,13 @@ $puzzleinput = Get-Content ./05input.txt
 $currentinstruction = 0
 $stepcount = 0
 
-while ($true) {
-    $destination = $currentinstruction + $puzzleinput[$currentinstruction]
+while ($currentinstruction -lt $puzzleinput.length) {
+    $currentinstruction += ([int]$puzzleinput[$currentinstruction])++
     # $puzzleinput -join ','
     # "cur = $currentinstruction"
     # "dst = $destination"
 
-    ([int]$puzzleinput[$currentinstruction])++
     $stepcount++
-
-    if ($destination -ge $puzzleinput.length) {break}
-    else {$currentinstruction = $destination}
 }
 
 $stepcount
